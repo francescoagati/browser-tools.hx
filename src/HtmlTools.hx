e37e7d25a0b0fa48ec4693c40c5cf50d0bb3b463
francesco agati <francescoagati1975@gmail.com>
2015-05-25 21:04:26 +0200

    Update HtmlTools.hx

--- src/HtmlTools.hx
+++ src/HtmlTools.hx
@@ -241,30 +241,3 @@ class HtmlTools {
   }
 
 }
-
-
-class StyleTools {
-
-	@:extern
-	public inline static function setClip(style:CSSStyleDeclaration,top:Int,left:Int,width:Int,height:Int) {
-		style.top = '${top}px';
-		style.left = '${left}px';
-		style.width = '${width}px';
-		style.height = '${height}px';
-		return style;
-	}
-
-	@:extern
-  public static inline function setColor(style:CSSStyleDeclaration,background:String,foreground:String) {
-    style.backgroundColor = background;
-		style.color = foreground;
-		return style;
-  }
-
-  @:extern
-  public static inline function setBackgroundImage(style:CSSStyleDeclaration,url:String) {
-    style.backgroundImage = 'url($url)';
-		return style;
-  }
-
-}
