0798afe1fdedc5476b32bf207efe663c93fa40f2
francesco agati <francescoagati1975@gmail.com>
2015-05-25 21:05:05 +0200

    Create AElement.hx

---
+++ src/AElement.hx
@@ -0,0 +1,94 @@
+@:forward abstract AElement(Element) from Element to Element {
+
+    public inline function new(el:Element) {
+        this = el;
+    }
+
+
+
+		@:from
+		public inline static function fromString(s:String):AElement {
+			var node = new AElement(Browser.document.createDivElement());
+      node.innerHTML = s;
+      return node;
+		}
+
+
+		@:from
+		public inline static function fromNode(node:Node):AElement {
+			return new AElement(untyped node);
+		}
+
+
+		@:from
+		public inline static function fromJQuery(jQ:JQuery):AElement {
+			return new AElement(untyped jQ.get(0));
+		}
+
+
+		@:op(A++)
+		public inline function show() {
+			this.style.display = 'inline-block';
+		}
+
+
+		@:op(A--)
+		public inline function hide() {
+			this.style.display = 'none';
+		}
+
+
+		@:op(A ^ B)
+		public inline function setHTMLSTring(s:String) {
+			this.innerHTML = s;
+		}
+
+
+		@:op(A ^ B)
+		public inline function setHTMLFromElement(element:Element) {
+			this.innerHTML = "";
+      this.appendChild(element);
+		}
+
+
+		@:op(A << B)
+		public inline function appendElementString(s:String) {
+			var node = Browser.document.createDivElement();
+			node.innerHTML = s;
+			this.appendChild(node);
+		}
+
+		@:op(A << B)
+		public inline function appendElement(el:Element) {
+			this.appendChild(el);
+		}
+
+		@:op(A >> B)
+		public inline function prependElement(el:Element) {
+			this.insertBefore(el, this.firstElementChild);
+		}
+
+		@:op(A >> B)
+		public inline function prependElementString(s:String) {
+			var node = Browser.document.createDocumentFragment();
+			node.textContent = s;
+			this.insertBefore(node, this.firstElementChild);
+		}
+
+    @:arrayAccess
+    public inline function get(key:String):String {
+       return this.getAttribute(key);
+    }
+
+		@:arrayAccess
+    public inline function set(key:String,value:String):Void {
+       this.setAttribute(key,value);
+    }
+
+
+		@:extern @:op(A / B)
+		public inline function search(selector:String) {
+			return lib.HtmlTools.query(selector,this);
+		}
+
+}
