9f3dea98d1c89363d08fa31e1090935d4f5bce32
Francesco Agati <fagati@veespo.com>
2015-07-15 15:50:00 +0200

    add browser_tools_log flag

--- src/browser_tools/Debug.hx
+++ src/browser_tools/Debug.hx
@@ -2,34 +2,54 @@ package browser_tools;
 
 class Debug {
   public inline static function console_log(obj:Dynamic,?msg:String = '') {
+    #if browser_tools_log
     js.Browser.console.log(msg,obj);
+    #end
   }
 
 
   public inline static function weblog_tic(obj:Dynamic,?msg:String = '') {
+    #if browser_tools_log
     Weblog.tic(untyped obj);
+    #end
   }
 
   public inline static function weblog_toc(obj:Dynamic,?msg:String = '') {
+    #if browser_tools_log
     Weblog.toc(untyped obj);
+    #end
   }
 
   public inline static function weblog_log(obj:Dynamic,?msg:String = '') {
+    #if browser_tools_log
     Weblog.log(untyped obj);
+    #end
   }
 
 
   public inline static function weblog_debug(obj:Dynamic,?msg:String = '') {
+    #if browser_tools_log
+    Weblog.debug(obj);
+    #end
+  }
+
+  public inline static function weblog_debug_json(obj:Dynamic,?msg:String = '') {
+    #if browser_tools_log
     Weblog.debug(untyped haxe.Json.stringify(obj));
+    #end
   }
 
   public inline static function weblog_inspect(obj:Dynamic,?msg:String = '') {
+    #if browser_tools_log
     Weblog.inspect(untyped obj);
+    #end
   }
 
 
   public inline static function alert_log(obj:Dynamic) {
+    #if browser_tools_log
     js.Browser.alert(haxe.Json.stringify(obj));
+    #end
   }
 
 
