c254c85a4515f9d7b16c29ce72c73ba16d323d70
Francesco Agati <fagati@veespo.com>
2015-07-15 14:29:09 +0200

    add weblog support

--- src/browser_tools/Debug.hx
+++ src/browser_tools/Debug.hx
@@ -5,9 +5,35 @@ class Debug {
     js.Browser.console.log(msg,obj);
   }
 
+
+  public inline static function weblog_tic(obj:Dynamic,?msg:String = '') {
+    Weblog.tic(obj);
+  }
+
+  public inline static function weblog_toc(obj:Dynamic,?msg:String = '') {
+    Weblog.toc(obj);
+  }
+
+  public inline static function weblog_log(obj:Dynamic,?msg:String = '') {
+    Weblog.log(obj);
+  }
+
+
+  public inline static function weblog_debug(obj:Dynamic,?msg:String = '') {
+    Weblog.debug(obj);
+  }
+
+  public inline static function weblog_inspect(obj:Dynamic,?msg:String = '') {
+    Weblog.inspect(obj);
+  }
+
+
   public inline static function alert_log(obj:Dynamic) {
     js.Browser.alert(haxe.Json.stringify(obj));
   }
+
+
+
 
 
 }
