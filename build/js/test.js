8b041b2c845055093ed726e8b858c7931bb5d766
Francesco Agati <fagati@veespo.com>
2015-12-18 17:02:55 +0100

    add animations tools

--- build/js/test.js
+++ build/js/test.js
@@ -1,7 +1,14 @@
 (function (console, $hx_exports) { "use strict";
 $hx_exports.transaction_check = $hx_exports.transaction_check || {};
+function $extend(from, fields) {
+	function Inherit() {} Inherit.prototype = from; var proto = new Inherit();
+	for (var name in fields) proto[name] = fields[name];
+	if( fields.toString !== Object.prototype.toString ) proto.toString = fields.toString;
+	return proto;
+}
 var Test = function() { };
 Test.main = function() {
+	new browser_$tools_animations_fps_Printer();
 };
 var async_$tools_Async = function() { };
 var transaction_$check = function() { };
@@ -52,6 +59,57 @@ transaction_$check.check_for_animations = $hx_exports.transaction_check.check_fo
 			__endIf_0();
 		} else __endIf_0();
 	},20);
+};
+var browser_$tools_animations_fps_Loop = function(cb) {
+	this._stop = false;
+	this.last_frame_time = null;
+	this.last_frame_time = new Date().getTime();
+	this.cb = cb;
+	this.event_loop(0);
+};
+browser_$tools_animations_fps_Loop.__interfaces__ = [async_$tools_Async];
+browser_$tools_animations_fps_Loop.prototype = {
+	event_loop: function(i) {
+		if(this._stop == true) return;
+		var now = new Date().getTime();
+		var fps = Math.floor(1000 / (now - this.last_frame_time));
+		this.last_frame_time = now;
+		this.cb(fps);
+		window.requestAnimationFrame($bind(this,this.event_loop));
+	}
+};
+var browser_$tools_animations_fps_Printer = function() {
+	this.fps = 0;
+	var _g = this;
+	var body = window.document.body;
+	var html = "\n      <div id=\"fps_printer\"\n        style=\"font-size:24px;padding:20px;position:fixed;top:0px;left:0px;z-index:99999999999999999999999999999;display:inline-block\"></div>\n    ";
+	var tmp;
+	var fragment = window.document.createDocumentFragment();
+	var div = window.document.createElement("div");
+	div.innerHTML = html;
+	tmp = div;
+	body.appendChild(tmp);
+	browser_$tools_animations_fps_Loop.call(this,function(value) {
+		_g.fps = value;
+	});
+	this.timer = new haxe_Timer(1000);
+	this.timer.run = function() {
+		var this1 = window.document.getElementById("fps_printer");
+		this1.innerHTML = "" + _g.fps + " fps";
+	};
+};
+browser_$tools_animations_fps_Printer.__super__ = browser_$tools_animations_fps_Loop;
+browser_$tools_animations_fps_Printer.prototype = $extend(browser_$tools_animations_fps_Loop.prototype,{
+});
+var haxe_Timer = function(time_ms) {
+	var me = this;
+	this.id = setInterval(function() {
+		me.run();
+	},time_ms);
+};
+haxe_Timer.prototype = {
+	run: function() {
+	}
 };
 var $_, $fid = 0;
 function $bind(o,m) { if( m == null ) return null; if( m.__id__ == null ) m.__id__ = $fid++; var f; if( o.hx__closures__ == null ) o.hx__closures__ = {}; else f = o.hx__closures__[m.__id__]; if( f == null ) { f = function(){ return f.method.apply(f.scope, arguments); }; f.scope = o; f.method = m; o.hx__closures__[m.__id__] = f; } return f; }
