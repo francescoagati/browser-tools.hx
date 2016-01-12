025d54df9a54f30b845bb994243be94a35dc989b
Francesco Agati <fagati@veespo.com>
2016-01-12 13:17:04 +0100

    add jsdeliver cdn

---
+++ src/browser_tools/assets/cdn/jsdeliver/Library.hx
@@ -0,0 +1,7 @@
+package browser_tools.assets.jsdeliver;
+
+@:enum
+@:forward
+abstract Library(String) from String to String {
+   var spinkit = 'https://cdn.jsdelivr.net/spinkit/1.2.5/spinkit.css';
+}
