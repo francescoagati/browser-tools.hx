cac077bb0a4998495f8a630cb5983f9a25d58f04
Francesco Agati <fagati@veespo.com>
2016-01-12 11:05:49 +0100

    add cdnjs and rawgit cdn elements

--- src/browser_tools/assets/cdn/cdnjs/Library.hx
+++ src/browser_tools/assets/cdn/cdnjs/Library.hx
@@ -3,6 +3,33 @@ package browser_tools.assets.cdn.cdnjs;
 @:enum
 @:forward
 abstract Library(String) from String to String {
-   var angular="https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.4.8/angular.min.js";
+   var angular = "https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.4.8/angular.min.js";
+   var angular_animate = 'https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.4.2/angular-animate.min.js';
+   var angular_ui_select = 'https://cdnjs.cloudflare.com/ajax/libs/angular-ui-select/0.13.2/select.min.js';
+   var angular_sanitize = 'https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.4.2/angular-sanitize.min.js';
+   var angular_route =  'https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.4.1/angular-route.min.js';
+   var angular_touch = 'https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.4.2/angular-touch.min.js';
+
+   var angular_ui_select_css = 'https://cdnjs.cloudflare.com/ajax/libs/angular-ui-select/0.12.0/select.min.css';
+   var metrics_graphics_css =  'https://cdnjs.cloudflare.com/ajax/libs/metrics-graphics/2.5.0/metricsgraphics.min.css';
+   var bootstrap_material_design = 'https://cdnjs.cloudflare.com/ajax/libs/bootstrap-material-design/0.3.0/css/material.min.css';
+   var nd_d3_css = 'https://cdnjs.cloudflare.com/ajax/libs/nvd3/1.7.1/nv.d3.min.css';
+
+   var animate_css = 'https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.3.0/animate.min.css';
+
+   var angular_aside_css =  'https://cdn.rawgit.com/dbtek/angular-aside/master/dist/css/angular-aside.min.css';
+
+   var d3 = 'https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.5/d3.min.js';
+   var jquery = '//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js';
+   var iscroll ='https://cdnjs.cloudflare.com/ajax/libs/iScroll/5.1.3/iscroll.min.js';
+
+   var nv_d3 = 'https://cdnjs.cloudflare.com/ajax/libs/nvd3/1.7.1/nv.d3.min.js';
+   var class_list = 'https://cdnjs.cloudflare.com/ajax/libs/classlist/2014.01.31/classList.min.js';
+
+   var topo_json = '//cdnjs.cloudflare.com/ajax/libs/topojson/1.6.19/topojson.min.js';
+   var metric_raphics = 'https://cdnjs.cloudflare.com/ajax/libs/metrics-graphics/2.5.0/metricsgraphics.min.js';
+   var angular_ui_bootstrap = 'https://cdnjs.cloudflare.com/ajax/libs/angular-ui-bootstrap/0.13.3/ui-bootstrap-tpls.min.js';
+
+
 
 }
---
+++ src/browser_tools/assets/cdn/raw_git/Library.hx
@@ -0,0 +1,16 @@
+package browser_tools.assets.cdn.cdnjs;
+
+@:enum
+@:forward
+abstract Library(String) from String to String {
+   var angular_iscroll = 'https://cdn.rawgit.com/mtr/angular-iscroll/master/dist/lib/angular-iscroll.min.js';
+   var angular_nvd3 = 'https://cdn.rawgit.com/krispo/angular-nvd3/master/dist/angular-nvd3.js';
+   var n3_charts_css =  'https://cdn.rawgit.com/n3-charts/pie-chart/master/css/pie-chart.css';
+   var datamaps = 'https://cdn.rawgit.com/francescoagati/datamaps/master/dist/datamaps.all.min.js';
+   var angular_datamaps = 'https://cdn.rawgit.com/dmachat/angular-datamaps/master/dist/angular-datamaps.min.js';
+   var d3_pie =  'https://cdn.rawgit.com/benkeen/d3pie/master/d3pie/d3pie.min.js';
+   var n3_charts = 'https://cdn.rawgit.com/n3-charts/pie-chart/master/dist/pie-chart.js';
+   var angular_metrics_graphics = 'https://cdn.rawgit.com/francescoagati/angular-metrics-graphics/master/src/scripts/directives/chart.js';
+   var flag_icon_css = 'https://cdn.rawgit.com/lipis/flag-icon-css/master/css/flag-icon.min.css';
+   var angular_fx_css =  'https://cdn.rawgit.com/720kb/angular-fx/master/dist/angular-fx.min.css';
+}
