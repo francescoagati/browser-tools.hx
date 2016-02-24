5550568b068e2d4ff622af36d1912367ae68a4fb
Francesco Agati <fagati@veespo.com>
2016-02-24 17:15:04 +0100

    fix for display object fix animation events

--- src/browser_tools/abstracts/DisplayAccessor.hx
+++ src/browser_tools/abstracts/DisplayAccessor.hx
@@ -12,10 +12,11 @@ abstract DisplayAccessor(Element) {
     public inline function wait_for_animation_end(?timeout:Int = 300,?property_detect:String = null,cb:Bool->Void) {
 
       var el = new AElement(this);
+
       var animation_ended = null;
 
-      var event_animation = Helper.get_event('AnimationEnd');
-      var event_transition = Helper.get_event('TransitionEnd');
+      var event_animation = browser_tools.events.Helper.get_event('AnimationEnd');
+      var event_transition = browser_tools.events.Helper.get_event('TransitionEnd');
       var check = false;
 
       function fn(e) {
@@ -24,7 +25,7 @@ abstract DisplayAccessor(Element) {
       };
 
       if (property_detect != null) {
-        Transaction.check_for_property_change(el,property_detect,function(response) {
+        browser_tools.animations.fps.Transaction.check_for_property_change(el,property_detect,function(response) {
           if (response == false) {
             animation_ended = false;
             fn(null);
@@ -34,12 +35,16 @@ abstract DisplayAccessor(Element) {
 
       function fn_animation_ended(e) {
         animation_ended = true;
-        el.removeEventListener(event_animation,fn);
-        el.removeEventListener(event_transition,fn);
+
+        el.removeEventListener(event_animation,fn_animation_ended);
+        el.removeEventListener(event_transition,fn_animation_ended);
         fn(e);
+
       }
 
       el.addEventListener(event_animation,fn_animation_ended);
       el.addEventListener(event_transition,fn_animation_ended);
+
     }
+
 }
